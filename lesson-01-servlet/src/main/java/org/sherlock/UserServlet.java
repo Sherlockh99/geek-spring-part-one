2b4cd05fc0977eaee070f9bb14e4e62dca37e44e
Andrew <sherlockh989@gmail.com>
2022-05-29 10:10:15 +0300

    Lesson 2.4:    Added relative links (относительные ссылки)

--- lesson-01-servlet/src/main/java/org/sherlock/UserServlet.java
+++ lesson-01-servlet/src/main/java/org/sherlock/UserServlet.java
@@ -50,8 +50,8 @@ public class UserServlet extends HttpServlet {
             for (User user : userRepository.findAll()) {
 
                 wr.println("<tr>");
-                wr.println("<th>" + user.getId() + "</th>");
-                wr.println("<th>" + user.getUsername() + "</th>");
+                wr.println("<td><a href='user/" + user.getId() + "'>" + user.getId() + "</a></td>");
+                wr.println("<td>" + user.getUsername() + "</td>");
                 wr.println("</tr>");
 
             }
