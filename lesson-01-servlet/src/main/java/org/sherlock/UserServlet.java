dff9982f0d3470647500598d0e05ef510bc29003
Andrew <sherlockh989@gmail.com>
2022-05-29 10:15:59 +0300

    Lesson 2.5:    Added absolute links

--- lesson-01-servlet/src/main/java/org/sherlock/UserServlet.java
+++ lesson-01-servlet/src/main/java/org/sherlock/UserServlet.java
@@ -50,7 +50,7 @@ public class UserServlet extends HttpServlet {
             for (User user : userRepository.findAll()) {
 
                 wr.println("<tr>");
-                wr.println("<td><a href='user/" + user.getId() + "'>" + user.getId() + "</a></td>");
+                wr.println("<td><a href='" + req.getContextPath() + "/user/" + user.getId() + "'>" + user.getId() + "</a></td>");
                 wr.println("<td>" + user.getUsername() + "</td>");
                 wr.println("</tr>");
 
